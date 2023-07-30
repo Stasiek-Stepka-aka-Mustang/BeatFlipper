f3edb56b9a67c339a2cdad4d931d14caadcf9934
Stanisław Stepka <139631524+Stasiek-Stepka-aka-Mustang@users.noreply.github.com>
2023-07-30 16:16:20 +0200

    Add files via upload

--- BeatFlipper/cmake-build-debug/cmake_install.cmake
+++ BeatFlipper/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/macbookpro/CLionProjects/BeatFlipper/BeatFlipper
+# Install script for directory: /Users/macbookpro/CLionProjects/BeatFlipper/BeatFlipper/BeatFlipper
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/macbookpro/CLionProjects/BeatFlipper/BeatFlipper/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/macbookpro/CLionProjects/BeatFlipper/BeatFlipper/BeatFlipper/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
